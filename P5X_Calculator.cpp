262ce2892931fa4209530597c0389651a9e0073d
Rich <HONGRICHARD89@GMAIL.COM>
2025-08-28 19:01:14 -0700

    add calculations for jewels in inventory

--- P5X_Calculator.cpp
+++ P5X_Calculator.cpp
@@ -7,17 +7,25 @@ int main(){
     int characterBanner;
     int metajewels = 150;
     int ownedMetajewels;
+    int neededMetajewels;
+    int banners;
 
     cout << "Select the character banner you are planning to roll on?(80 or 110) ";
     cin >> characterBanner;
+
+    cout << "How many banners are you trying to save for? ";
+    cin >> banners;
+
     if(characterBanner == 80){
-        metajewels = metajewels * 80 * 2;
+
+        banners = banners * 2;
+        metajewels = metajewels * 80 * banners;
         cout << "For a guarantee, you will need " << metajewels << " metajewels.\n";
 
     }
     else if (characterBanner == 110)
     {
-        metajewels = metajewels * 110;
+        metajewels = metajewels * 110 * banners;
         cout << "For a guarantee, you will need " << metajewels << " metajewels.\n";
     }
     else{
@@ -27,7 +35,9 @@ int main(){
     
     cout << "How many Metajewels do you currently own? ";
     cin >> ownedMetajewels;
+    neededMetajewels = metajewels - ownedMetajewels;
+    cout << "You need " << neededMetajewels << " to reach the character(s) you desire.\n";
 
-
+    system("pause");
     return 0;
 }
