f3ad4266e913e6651202834ba22f8b5586818955
Rich <HONGRICHARD89@GMAIL.COM>
2025-08-29 20:56:05 -0700

    add Cognigems to calculations

--- P5X_Calculator.cpp
+++ P5X_Calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ int main(){
     int ownedMetajewels;
     int neededMetajewels;
     int banners;
+    int phantomTickets;
+    int phantomJewels;
+    int cognigems;
 
     cout << "Select the character banner you are planning to roll on?(80 or 110) ";
     cin >> characterBanner;
@@ -36,6 +40,15 @@ int main(){
     cout << "How many Metajewels do you currently own? ";
     cin >> ownedMetajewels;
     neededMetajewels = metajewels - ownedMetajewels;
+    cout << "How many phantom tickets do you own? ";
+    cin >> phantomTickets;
+    phantomJewels = phantomTickets * 150;
+    neededMetajewels = neededMetajewels - phantomJewels;
+    cout <<  "How many Violet Cognigems do you own? ";
+    cin >> cognigems;
+    cognigems = floor(cognigems / 10);
+    cognigems = cognigems * 100;
+    neededMetajewels = neededMetajewels - cognigems;
     cout << "You need " << neededMetajewels << " to reach the character(s) you desire.\n";
 
     system("pause");
